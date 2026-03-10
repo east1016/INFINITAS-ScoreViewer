df9d9d0c5f57afa4bf3e3baa9b1f07e32c8535bf
east1016 <floweryoshitakaeast16@gmail.com>
2026-03-10 23:26:26 +0900

    chore: GitHub Actionsデプロイワークフローを削除

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import sitemap from 'vite-plugin-sitemap';
 
-export default defineConfig(({ mode }) => {
-  // baseプロパティに設定する値
-  let base = "/"
-
-  // 本番時に適用させるbaseの値
-  if (mode === "production") {
-    base = "/INFINITAS-ScoreViewer/"
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    target: 'esnext',
+  },
+  define: {
+    'process.env.BASE_URL': JSON.stringify('/'),
+  },
+  server: {
+    port: 3000
   }
-
-  return {
-    plugins: [
-      react(),
-      sitemap({
-        hostname: 'https://chinimuruhi.github.io/INFINITAS-ScoreViewer',
-        outDir: 'dist',
-        basePath: "/INFINITAS-ScoreViewer",
-        exclude: ['/404', '/google9de41a3cfb1f4454'],
-      }),
-    ],
-    // アセットなどのパスを変換するベースとなるパス
-    base,
-    // ビルドの出力先
-    build: {
-      outDir: 'dist',
-      target: 'esnext',
-    },
-    define: {
-      'process.env.BASE_URL': JSON.stringify(base),
-    },
-    server: {
-      port: 3000
-    }
-  };
-})
+});
